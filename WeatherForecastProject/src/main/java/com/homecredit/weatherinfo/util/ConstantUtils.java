eabf5b61cc4ed848e9e3313e92a26feba2cd3cbf
jekjek03 <43834471+jekjek03@users.noreply.github.com>
2018-10-09 06:19:39 +0800

    Delete ConstantUtils.java

--- WeatherForecastProject/src/main/java/com/homecredit/weatherinfo/util/ConstantUtils.java
+++
@@ -1,9 +0,0 @@
-package com.homecredit.weatherinfo.util;
-
-public class ConstantUtils {
-
-	public static final String API_KEY = "c406495527522c36fcf6236cd5a757f8";
-	public static final String REQUEST_URL = "http://localhost:8081/weatherforecasts";
-	public static final String OPEN_WEATHER_MAP_URL = "http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={key}";
-	
-}
